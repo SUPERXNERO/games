926d839c1a68a2f909ef33f76497fc42501ece2f
SUPERXNERO <110180629+SUPERXNERO@users.noreply.github.com>
2024-10-16 23:42:16 +0300

    update app.jsx

--- app.jsx
+++ app.jsx
@@ -3,12 +3,7 @@ import React, { useState, useContext } from 'react';
 const App = () => {
   const [state, setState] = useState();
   return (
-    <div>
-      <h1>Hello, React from an external file!</h1>
-      <p>
-        This is a React component loaded from a separate JSX file.
-    </p>
-    </div>
+    
   );
 };
 
