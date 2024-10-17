61064e4e39278d1c7e2f22c8283269812e395f02
SUPERXNERO <110180629+SUPERXNERO@users.noreply.github.com>
2024-10-17 15:02:12 +0300

    update App.jsx

--- App.jsx
+++ App.jsx
@@ -15,7 +15,7 @@ const App = () => {
   return (
     <LangContext.Provider value={{ lang, setLang }}>
       <ThemeContext.Provider value={{ theme, setTheme }}>
-        hello vercel!
+        hello time!
       </ThemeContext.Provider>
     </LangContext.Provider>
   );
